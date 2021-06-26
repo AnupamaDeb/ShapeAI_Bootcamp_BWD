59fc449e2acc8c8a5f5932a8762c4ee063b1243f
AnupamaDeb <83230767+AnupamaDeb@users.noreply.github.com>
2021-06-26 17:10:55 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>It was an amazing basic web development Bootcamp taken up by Shaurya Sinha Sir. We Covered everything from scratch including JavaScript React.js, HTML.</p>
+    </div>
+  );
+}
+
+export default Info;
